8c61de117bb476384a1df729b37ff8be4288c7d8
studenthelp360 <classwork.922@gmail.com>
2026-08-07 14:04:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
 
 export const CONFIG_APP = {
   // Your Supabase Project API URL (e.g. https://xxxxxx.supabase.co)
-  SUPABASE_URL: "YOUR_SUPABASE_URL",
+  SUPABASE_URL: "https://ieiqmvyuhdzbalxvfbzv.supabase.co",
 
   // Your Supabase Public Anonymous Key (found in API settings)
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaXFtdnl1aGR6YmFseHZmYnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMzUyNTgsImV4cCI6MjEwMTYxMTI1OH0.NwZ1Tj38z0AQLo0jxkggL2i-WpSo00UD76mR4A90hpM"
 };
